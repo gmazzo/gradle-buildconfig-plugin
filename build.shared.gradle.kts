c7a88a8b7fd0a8d1afa42fe4058deb3c75367104
Guillermo Mazzola <gmazzo65@gmail.com>
2019-03-20 23:23:06 -0300

    Fixed issue of missing "javax.annotation.Generated" when non on JVM projects

--- build.shared.gradle.kts
+++ build.shared.gradle.kts
@@ -5,7 +5,7 @@ allprojects {
     }
 
     group = "com.github.gmazzo"
-    version = "1.3.5"
+    version = "1.3.6"
 
     project.plugins.withType(JavaPlugin::class.java) {
         dependencies.add("testImplementation", "junit:junit:4.12")
