09ac573440a13a75a981d7a734be955a70361fb5
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2023-04-24 18:27:40 +0200

    Refactored bindings

--- plugin/src/main/kotlin/com/github/gmazzo/gradle/plugins/internal/bindings/KotlinHandler.kt
+++ plugin/src/main/kotlin/com/github/gmazzo/gradle/plugins/internal/bindings/KotlinHandler.kt
@@ -3,22 +3,22 @@ package com.github.gmazzo.gradle.plugins.internal.bindings
 import com.github.gmazzo.gradle.plugins.BuildConfigClassSpec
 import com.github.gmazzo.gradle.plugins.BuildConfigExtension
 import com.github.gmazzo.gradle.plugins.generators.BuildConfigKotlinGenerator
-import org.gradle.api.DomainObjectCollection
 import org.gradle.api.Named
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.api.file.SourceDirectorySet
 
 internal class KotlinHandler(
-    private val project: Project,
+    project: Project,
     private val extension: BuildConfigExtension
-) : PluginBindingHandler<Named> {
+) : PluginBindingHandler<Named> { // TODO should be KotlinSourceSet but fails on tests (but not from external project)
 
-    override val sourceSets: DomainObjectCollection<Named>
-        get() = with(project.extensions.getByName("kotlin")) {
-            @Suppress("UNCHECKED_CAST")
-            javaClass.getMethod("getSourceSets")
-                .invoke(this) as DomainObjectCollection<Named>
-        }
+    // project.extensions.getByName<KotlinSourceSetContainer>("kotlin").sourceSets
+    override val sourceSets = with(project.extensions.getByName("kotlin")) {
+        @Suppress("UNCHECKED_CAST")
+        javaClass.getMethod("getSourceSets")
+            .invoke(this) as NamedDomainObjectContainer<Named>
+    }
 
     override fun nameOf(sourceSet: Named): String = sourceSet.name
 
@@ -26,6 +26,7 @@ internal class KotlinHandler(
         extension.generator.convention(BuildConfigKotlinGenerator())
     }
 
+    // sourceSet.kotlin.srcDir(spec.generateTask)
     override fun onSourceSetAdded(sourceSet: Named, spec: BuildConfigClassSpec) {
         (sourceSet.javaClass.getMethod("getKotlin")
             .invoke(sourceSet) as SourceDirectorySet)
--- plugin/src/main/kotlin/com/github/gmazzo/gradle/plugins/internal/bindings/KotlinMultiplatformHandler.kt
+++ plugin/src/main/kotlin/com/github/gmazzo/gradle/plugins/internal/bindings/KotlinMultiplatformHandler.kt
@@ -1,19 +1,12 @@
 package com.github.gmazzo.gradle.plugins.internal.bindings
 
-import com.github.gmazzo.gradle.plugins.BuildConfigExtension
 import org.gradle.api.Named
-import org.gradle.api.Project
 import org.gradle.api.tasks.SourceSet
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
 internal class KotlinMultiplatformHandler(
     private val kotlinHandler: KotlinHandler
 ) : PluginBindingHandler<Named> by kotlinHandler {
-
-    constructor(
-        project: Project,
-        extension: BuildConfigExtension
-    ) : this(KotlinHandler(project, extension))
 
     override fun nameOf(sourceSet: Named) = when (val name = kotlinHandler.nameOf(sourceSet)) {
         KotlinSourceSet.COMMON_MAIN_SOURCE_SET_NAME -> SourceSet.MAIN_SOURCE_SET_NAME
