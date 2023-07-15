7083203182288891e904099f8f5ab43d75e049c1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-15 14:39:55 +0100

    Change convention usage to extension.

--- plugin/src/main/kotlin/com/github/gmazzo/gradle/plugins/internal/bindings/JavaHandler.kt
+++ plugin/src/main/kotlin/com/github/gmazzo/gradle/plugins/internal/bindings/JavaHandler.kt
@@ -4,16 +4,16 @@ import com.github.gmazzo.gradle.plugins.BuildConfigExtension
 import com.github.gmazzo.gradle.plugins.BuildConfigSourceSet
 import com.github.gmazzo.gradle.plugins.generators.BuildConfigJavaGenerator
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.SourceSet
-import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.kotlin.dsl.the
+import org.gradle.kotlin.dsl.getByName
 
 internal class JavaHandler(
     project: Project,
     private val extension: BuildConfigExtension
 ) : PluginBindingHandler<SourceSet> {
 
-    override val sourceSets = project.the<SourceSetContainer>()
+    override val sourceSets = project.extensions.getByName<JavaPluginExtension>("java").sourceSets
 
     override fun nameOf(sourceSet: SourceSet): String = sourceSet.name
 
