3e15890e343923a114eca4dfbcf9cf9c3bd06437
Guillermo Mazzola <gmazzo65@gmail.com>
2023-12-11 23:50:41 +0100

    Implemented type safe DSL

--- demo-project/kts/build.gradle.kts
+++ demo-project/kts/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.gmazzo.gradle.plugins.BuildConfigField
 import com.github.gmazzo.gradle.plugins.generators.BuildConfigGenerator
 import com.github.gmazzo.gradle.plugins.generators.BuildConfigGeneratorSpec
 import java.io.FileOutputStream
@@ -31,7 +32,9 @@ buildConfig {
     buildConfigField("String?", "OPTIONAL", "null")
     buildConfigField("long", "BUILD_TIME", "${System.currentTimeMillis()}L")
     buildConfigField("boolean", "FEATURE_ENABLED", "${true}")
-    buildConfigField("kotlin.IntArray", "MAGIC_NUMBERS", "intArrayOf(1, 2, 3, 4)")
+    buildConfigField("int[]", "MAGIC_NUMBERS", "intArrayOf(1, 2, 3, 4)")
+    buildConfigField("Integer[]", "MAGIC_NUMBERS2", "intArrayOf(1, 2, 3, 4)")
+    buildConfigField("kotlin.IntArray", "MAGIC_NUMBERS3", "intArrayOf(9, 10)")
     buildConfigField("com.github.gmazzo.buildconfig.demos.kts.SomeData", "MY_DATA", "SomeData(\"a\",1)")
 
     sourceSets["test"].buildConfigField("String", "TEST_CONSTANT", "\"aTestValue\"")
@@ -74,7 +77,7 @@ val propertiesSS = buildConfig.sourceSets.register("properties") {
             spec.outputDir.mkdirs()
 
             Properties().also { props ->
-                spec.fields.forEach { props.setProperty(it.name, it.value.get()) }
+                spec.fields.forEach { props.setProperty(it.name, (it.value.get() as BuildConfigField.Expression).value) }
                 props.storeToXML(FileOutputStream(File(spec.outputDir, "${spec.className}.xml")), null)
             }
         }
