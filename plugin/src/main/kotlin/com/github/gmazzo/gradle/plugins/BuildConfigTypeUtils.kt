0331e5ce529c1c37a437e8707ca1606ad4d9465f
Guillermo Mazzola <gmazzo65@gmail.com>
2023-12-12 02:38:55 +0100

    Fixed generation issues with new DSL

---
+++ plugin/src/main/kotlin/com/github/gmazzo/gradle/plugins/BuildConfigTypeUtils.kt
@@ -0,0 +1,32 @@
+package com.github.gmazzo.gradle.plugins
+
+private val regEx = "(.*?)(\\[])?(\\?)?".toRegex()
+
+internal fun String.parseTypename(): Triple<String, Boolean, Boolean> = regEx.matchEntire(this)?.let {
+    val (type, array, nullable) = it.destructured
+
+    Triple(type, array.isNotEmpty(), nullable.isNotEmpty())
+} ?: Triple(this, false, false)
+
+internal fun Any?.asVarArg(): Array<*> = when (this) {
+    is Array<*> -> this
+    is ByteArray -> toTypedArray()
+    is ShortArray-> toTypedArray()
+    is CharArray-> toTypedArray()
+    is IntArray-> toTypedArray()
+    is LongArray-> toTypedArray()
+    is FloatArray-> toTypedArray()
+    is DoubleArray -> toTypedArray()
+    is BooleanArray -> toTypedArray()
+    is List<*> -> toTypedArray()
+    is Iterable<*> -> toList().toTypedArray()
+    else -> arrayOf(this)
+}
+
+internal val Any.bestGuessType get() = when (this) {
+    is Map<*, *> -> Map::class.java
+    is Set<*> -> Set::class.java
+    is Iterable<*> -> List::class.java
+    is CharSequence -> String::class.java
+    else -> this::class.java
+}
