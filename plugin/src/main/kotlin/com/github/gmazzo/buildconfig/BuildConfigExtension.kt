18e328f809714fc694528d6e79897c95b60a38b1
Guillermo Mazzola <gmazzo65@gmail.com>
2024-01-02 22:53:38 +0100

    Generate build config class at Sync

--- plugin/src/main/kotlin/com/github/gmazzo/buildconfig/BuildConfigExtension.kt
+++ plugin/src/main/kotlin/com/github/gmazzo/buildconfig/BuildConfigExtension.kt
@@ -2,8 +2,11 @@ package com.github.gmazzo.buildconfig
 
 import org.gradle.api.Action
 import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.provider.Property
 
 interface BuildConfigExtension : BuildConfigSourceSet {
+
+    val generateAtSync: Property<Boolean>
 
     val sourceSets: NamedDomainObjectContainer<out BuildConfigSourceSet>
 
