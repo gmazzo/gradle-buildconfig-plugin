6c1523bb8049ac279f8043462abe3344d5158769
Guillermo Mazzola <gmazzo65@gmail.com>
2025-11-06 15:54:43 +0100

    Fixed breaking changes

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -58,7 +58,7 @@ gradlePlugin {
 }
 
 mavenPublishing {
-    publishToMavenCentral("CENTRAL_PORTAL", automaticRelease = true)
+    publishToMavenCentral(automaticRelease = true)
 
     pom {
         name = "${rootProject.name}-${project.name}"
