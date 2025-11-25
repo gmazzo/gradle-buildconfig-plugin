afcdf947e545f8140aad80fe6a5f5d8d476aaecd
Guillermo Mazzola <gmazzo65@gmail.com>
2025-11-25 16:28:28 +0100

    Extension constructor refactor (#354)

--- plugin/src/main/kotlin/com/github/gmazzo/buildconfig/internal/BuildConfigExtensionInternal.kt
+++ plugin/src/main/kotlin/com/github/gmazzo/buildconfig/internal/BuildConfigExtensionInternal.kt
@@ -3,7 +3,7 @@ package com.github.gmazzo.buildconfig.internal
 import com.github.gmazzo.buildconfig.BuildConfigExtension
 import org.gradle.api.NamedDomainObjectContainer
 
-internal interface BuildConfigExtensionInternal : BuildConfigExtension {
+internal interface BuildConfigExtensionInternal : BuildConfigExtension, BuildConfigSourceSetInternal {
 
     override val sourceSets: NamedDomainObjectContainer<out BuildConfigSourceSetInternal>
 
--- plugin/src/main/kotlin/com/github/gmazzo/buildconfig/internal/DefaultBuildConfigExtension.kt
+++ plugin/src/main/kotlin/com/github/gmazzo/buildconfig/internal/DefaultBuildConfigExtension.kt
@@ -1,10 +1,22 @@
 package com.github.gmazzo.buildconfig.internal
 
+import javax.inject.Inject
 import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.SourceSet
+import org.gradle.kotlin.dsl.domainObjectContainer
 
-internal abstract class DefaultBuildConfigExtension(
+internal abstract class DefaultBuildConfigExtension private constructor(
     override val sourceSets: NamedDomainObjectContainer<BuildConfigSourceSetInternal>,
-    defaultSourceSet: BuildConfigSourceSetInternal
+    val defaultSourceSet: BuildConfigSourceSetInternal = sourceSets.maybeCreate(SourceSet.MAIN_SOURCE_SET_NAME)
 ) : BuildConfigExtensionInternal,
     BuildConfigSourceSetInternal by defaultSourceSet,
-    GroovyNullValueWorkaround()
+    GroovyNullValueWorkaround() {
+
+    @Inject
+    @Suppress("UNCHECKED_CAST")
+    constructor(objects: ObjectFactory): this(
+        objects.domainObjectContainer(DefaultBuildConfigSourceSet::class) as NamedDomainObjectContainer<BuildConfigSourceSetInternal>,
+    )
+
+}
