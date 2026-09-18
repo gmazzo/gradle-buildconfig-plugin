15d1274961055a0547e725568da0c00c9b0d267c
Guillermo Mazzola <gmazzo65@gmail.com>
2026-09-18 12:16:01 +0200

    Apply batched suggestions from code review
    Co-authored-by: Guillermo Mazzola <gmazzo65@gmail.com>

--- plugin/src/main/kotlin/com/github/gmazzo/buildconfig/internal/bindings/JavaBinder.kt
+++ plugin/src/main/kotlin/com/github/gmazzo/buildconfig/internal/bindings/JavaBinder.kt
@@ -8,11 +8,12 @@ import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.the
 
 internal object JavaBinder {
 
     fun Project.configure(extension: BuildConfigExtension) {
-        val sourceSets: SourceSetContainer = extensions.getByType(SourceSetContainer::class.java)
+        val sourceSets = the<SourceSetContainer>()
 
         sourceSets.configureEach { sourceSet ->
             val spec = extension.sourceSets.maybeCreate(sourceSet.name)
